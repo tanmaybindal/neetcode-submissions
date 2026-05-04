dd10d90adc43f0d8d9967ddd1c6b6b72625beb9a
Tanmay Bindal <bindaltanmay@gmail.com>
2026-05-04 13:17:36 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        n = len(s)
+        m = len(t)
+        if m > n:
+            return ""
+
+        freq_map = defaultdict(int)
+
+        l = 0
+        r = 0
+        min_len = float("inf")
+
+        remaining = 0
+        start_idx = 0
+
+        for ch in t:
+            freq_map[ch] += 1
+            remaining += 1
+
+        while r < n:
+            val = s[r]
+            if freq_map[val] > 0:
+                remaining -= 1
+            freq_map[val] -= 1
+            r += 1
+
+            while remaining == 0:
+                if r - l < min_len:
+                    min_len = r - l
+                    start_idx = l
+                l_val = s[l]
+                freq_map[l_val] += 1
+                if freq_map[l_val] > 0:
+                    remaining += 1
+                l += 1
+
+        if min_len == float("inf"):
+            return ""
+        return s[start_idx : start_idx + min_len]
